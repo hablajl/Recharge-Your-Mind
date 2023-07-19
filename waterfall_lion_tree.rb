9e4881ed328a7b2f8cf7d224247fd5de2544bce0
hablajl <136637926+hablajl@users.noreply.github.com>
2023-07-19 15:56:20 +0700

    Improve data validation

---
+++ waterfall_lion_tree.rb
@@ -0,0 +1,501 @@
+#Recharge Your Mind
+
+#1
+def meditation
+  puts "Start your day with mindfulness meditation."
+end
+
+#2
+def positive_thinking
+  puts "Focus on positive thoughts to keep you motivated throughout the day."
+end
+
+#3
+def step_back
+  puts "Break away from stress and take a step back to breathe."
+end
+
+#4
+def gratitude
+  puts "Cultivate an attitude of gratitude for the smallest of things and find contentment."
+end
+
+#5
+def unplug
+  puts "Unplug from technology and give yourself some much needed mental freedom."
+end
+
+#6
+def sleep_well 
+  puts "A good night's sleep can do wonders for the mind so take the time to get some rest."
+end
+
+#7
+def physical_activity
+  puts "Engage in physical activity and exercise to keep your mind sharp and focused."
+end
+
+#8
+def limit_stimulation
+  puts "Limit outside stimulation to give your brain a rest and prevent mental exhaustion."
+end 
+
+#9
+def problem_solving
+  puts "Challenge yourself with puzzles or problem solving activities to keep your mind active."
+end
+
+#10
+def brain_teasers
+  puts "Train your brain with a few brain teasers to help keep it alert and sharp."
+end
+
+#11
+def read
+  puts "Reading is essential to learning, so take some time to read something new and interesting."
+end
+
+#12
+def socialize
+  puts "Take a break to socialize and connect with friends or family to give your mind a boost."
+end
+
+#13
+def journal
+  puts "Write in a journal to help clear your mind and organize your thoughts in a healthy way."
+end
+
+#14
+def yoga
+  puts "Yoga can be a great way to relax and refocus your mind. Give it a try!"
+end
+
+#15
+def music
+  puts "Listen to calming music to bring peace and tranquility to your mind."
+end
+
+#16
+def nature
+  puts "Spend some time outdoors in nature to allow yourself to observe, reset and appreciate the beauty of life."
+end
+
+#17
+def hobbies
+  puts "Pick up a hobby or explore a new interest to stimulate your mind and jumpstart creativity."
+end
+
+#18
+def mental_check
+  puts "Check in with your mental state regularly throughout the day by taking a few moments to pause and refocus."
+end
+
+#19
+def laughter
+  puts "Laughter is good for the soul, so take some time to throw on a funny show or movie to lighten the mood."
+end
+
+#20
+def affirmations
+  puts "Start your day by writing down your affirmations to remember your self worth and speak positivity into your life."
+end
+
+#21
+def creative
+  puts "Be creative, try something new, and break the routine. It can do wonders for the mind."
+end
+
+#22
+def plan
+  puts "Take a few moments to plan out your days in advance and create a timeline for success."
+end
+
+#23
+def visualization
+  puts "Visualize your goals and dreams and keep them in the front of your mind for motivation."
+end
+
+#24
+def stretch
+  puts "Don't forget to stretch throughout the day to increase blood flow to the brain and ease any tension."
+end
+
+#25
+def mindfulness
+  puts "Practice mindfulness and take a few moments of self-awareness and internal reflection."
+end
+
+#26
+def joy
+  puts "Allow yourself to feel joy and embrace your passions. Happiness is essential for mental health."
+end
+
+#27
+def down_time
+  puts "Allow yourself ample down time to just relax and be in the moment with no agenda or stress."
+end
+
+#28
+def take_breaks
+  puts "Take breaks in between tasks throughout the day to give your mind some space to reset."
+end
+
+#29
+def nap
+  puts "When you need a boost, try taking a nap to relax and reboot."
+end
+
+#30
+def fun
+  puts "Have some fun and do something spontaneous. Don't forget to leave room for spontaneity in your life."
+end
+
+#31
+def positive_people
+  puts "Surround yourself with positive people who bring out the best in you and contribute to an uplifting environment."
+end
+
+#32
+def challenging
+  puts "Step outside of your comfort zone and challenge yourself with something new."
+end
+
+#33
+def nourish
+  puts "Nourish your body by eating healthy foods that are full of nutrients and vitamins essential for mental clarity."
+end
+
+#34
+def hydrate
+  puts "Remember to drink plenty of water throughout the day to stay hydrated and clear-headed."
+end
+
+#35
+def disconnect
+  puts "Turn off your devices from time to time and disconnect to detach from all of the chaos going on in the digital world."
+end
+
+#36
+def quiet_time
+  puts " schedue some quiet time alone to process thoughts and revisit these ideas of recharging your mind."
+end
+
+#37
+def break_routine
+  puts "Break the daily routine and do something unexpected to give your mind a break and allow for some creativity."
+end
+
+#38
+def be_present
+  puts "Be present and live in the moment. Enjoy the beauty of the everyday simplicity and appreciate all that you have."
+end
+
+#39
+def spiritual
+  puts "Explore your spiritual growth and discover the power of faith and inner peace."
+end
+
+#40
+def technology_free
+  puts "Create a technology free zone and disconnect from the distractions of the digital world."
+end
+
+#41
+def mindful_eating
+  puts "Focus on mindful eating to bring yourself back to the present moment and take a break from stress."
+end
+
+#42
+def art
+  puts "Express yourself through art and allow creative freedom which can do wonders for a stressed mind."
+end
+
+#43
+def prioritize
+  puts "Prioritize your tasks and organize your time accordingly to create a sustainable and achievable plan of action."
+end
+
+#44
+def hobbies_outside
+  puts "Try something new outside of work and hobbies to give your mind a chance to explore and discover something new."
+end
+
+#45
+def introspection
+  puts "Engage in introspection and reflect on the current state of your life to reconnect with yourself and your intentions."
+end
+
+#46
+def mindfulness_meditation
+  puts "Practice mindfulness meditation to bring yourself to the present moment and take a break from the noise of the world."
+end
+
+#47
+def set_goals
+  puts "Set goals and make a plan for achieving them. It can do wonders for providing the motivation to keep pushing forward."
+end
+
+#48
+def nourish_mind
+  puts "Nourish your mind with healthy activities that contribute to inner peace and mental well-being."
+end
+
+#49
+def listen
+  puts "Listen more and talk less to create space for yourself and others to be heard and understood."
+end
+
+#50
+def happiness
+  puts "Seek happiness from within and find joy in life's simple moments."
+end
+
+#51
+def unwind
+  puts "Unwind and relax at the end of the day to help clear your mind for a good night's sleep."
+end
+
+#52
+def talk_therapy
+  puts "Go to talk therapy if you need to and don't be afraid to seek professional help when it's necessary."
+end
+
+#53
+def self_care
+  puts "Make time for self-care and take a break from your daily routine to give yourself a chance to recharge."
+end
+
+#54
+def nature_walk
+  puts "Take a nature walk and observe the beauty of the world around you. It can help ground you and bring some much needed peace of mind."
+end
+
+#55
+def practice_patience
+  puts "Practice patience and take things one step at a time to avoid getting overwhelmed."
+end
+
+#56
+def calming_activities
+  puts "Engage in calming activities such as journaling, yoga, or meditation to help center and soothe your mind."
+end
+
+#57
+def be_mindful
+  puts "Be mindful of your thoughts and practice self-love and positivity to bring happiness into your life and contribute to overall well-being."
+end
+
+#58
+def positive_self_talk
+  puts "Change your negative self-talk into positive affirmations to help boost your self-confidence and belief in yourself."
+end
+
+#59
+def kindness
+  puts "Practice random acts of kindness and show your gratitude for the people in your life."
+end
+
+#60
+def organized
+  puts "Become organized and create a plan of action for yourself that will help bring structure to your day and clear your mind."
+end
+
+#61
+def take_time
+  puts "Take the time to appreciate the little things and be thankful for all that you have in your life."
+end
+
+#62
+def mind_body
+  puts "Connect your mind and body and create a sense of balance and harmony in your life."
+end
+
+#63
+def creative_outlet
+  puts "Find an outlet for creativity and allow yourself to explore your passions and interests."
+end
+
+#64
+def treat_yourself
+  puts "Treat yourself to something special and take the time to indulge in something you enjoy."
+end
+
+#65
+def relaxation_techniques
+  puts "Learn relaxation techniques to help calm your mind and body when facing moments of stress and anxiety."
+end
+
+#66
+def natural_remedies
+  puts "Explore natural remedies to help reduce stress and enhance well-being."
+end
+
+#67
+def emotional_intelligence
+  puts "Develop emotional intelligence to cultivate self-awareness and understand the impact of emotions on mental health."
+end
+
+#68
+def conscious_awareness
+  puts "Create conscious awareness of your thoughts and feelings to guide you through difficult moments."
+end
+
+#69
+def get_outside
+  puts "Get outside more and enjoy your surroundings to clear your head and reset."
+end
+
+#70
+def affirm_strength
+  puts "Affirm your strength and recognize your accomplishments as a way to keep your motivation high."
+end
+
+#71
+def manage_stress
+  puts "Learn how to manage stress and find healthy ways to channel your energy."
+end
+
+#72
+def healthy_habits
+  puts "Create healthy habits and make positive changes to improve your overall quality of life."
+end
+
+#73
+def good_nutrition
+  puts "Eat a balanced and healthy diet to give your body the nutrition it needs for optimal mental health."
+end
+
+#74
+def boundary_setting
+  puts "Set boundaries with yourself and others to create a sense of safety and reduce stress levels."
+end
+
+#75
+def avoid_burnout
+  puts "Take proactive steps to avoid burnout and create a plan to keep yourself from overworking and overexerting yourself."
+end
+
+#76
+def vent
+  puts "Vent and talk to someone about your feelings as a way to let go of stress and anxiety."
+end
+
+#77
+def reduce_screen_time
+  puts "Reduce your screen time to help alleviate stress and increase productivity."
+end
+
+#78
+def simplify
+  puts "Simplify your life by clearing out the clutter and organizing your space to maintain a clear and focused mind."
+end
+
+#79
+def meaningful_connections
+  puts "Seek meaningful connections and cultivate lasting relationships to provide a source of emotional support."
+end
+
+#80
+def focus
+  puts "Focus on the present and cultivate patience to handle unexpected changes and obstacles with ease."
+end
+
+#81
+def go_slow
+  puts "Go slow and take your time with decisions to avoid making rash choices that could cause more harm than good."
+end
+
+#82
+def stress_reliever
+  puts "Find a stress reliever that works for you and make it part of your routine to help keep any stress at bay."
+end
+
+#83
+def travel
+  puts "Travel and explore new places to discover something new and gain perspective on life."
+end
+
+#84
+def body_awareness
+  puts "Increase your body awareness and become conscious of the signs of stress and learn how to respond accordingly."
+end
+
+#85
+def positive_affirmations
+  puts "Practice positive affirmations and remember to speak words of encouragement to yourself."
+end
+
+#86
+def connect_with_nature
+  puts "Connect with nature and appreciate the beauty of the outdoors to find a sense of peace and serenity."
+end
+
+#87
+def clear_mind
+  puts "Clear your mind and take a few moments to just be still and relax."
+end
+
+#88
+def let_go
+  puts "Let go of what no longer serves you and accept the things you cannot control."
+end
+
+#89
+def let_it_be
+  puts "Let it be and practice self-love and compassion to bring harmony back to your mental state."
+end
+
+#90
+def night_time_ritual
+  puts "Create a night time ritual to signal to your body and mind that its time to rest and recharge."
+end
+
+#91
+def talk_to_yourself
+  puts "Talk to yourself like you would talk to a friend and practice self-care and kindness."
+end
+
+#92
+def slow_down
+  puts "Slow down and live in the present moment to appreciate the beauty of life."
+end
+
+#93
+def gentle_reminders
+  puts "Set gentle reminders throughout the day to keep your mind focused and on track."
+end
+
+#94
+def relaxation_time
+  puts "Schedule relaxation time and give yourself permission to take a break and just be."
+end
+
+#95
+def self_improvement
+  puts "Focus on self-improvement and take the time to reflect on what you have learned and how you can grow from these experiences."
+end
+
+#96
+def power_of_yes
+  puts "Embrace the power of the word 'yes' and try something new that you may not have done before."
+end
+
+#97
+def pick_up_a_book
+  puts "Pick up a book and explore something new and stimulating to exercise the mind."
+end
+
+#98
+def acceptance
+  puts "Accept what is and be gentle with yourself. Nobody is perfect."
+end
+
+#99
+def healthy_habits
+  puts "Establish healthy habits and make small changes to improve your overall emotional and mental health."
+end
+
+#100
+def practice_contemplation
+  puts "Practice contemplation and take a few moments to reflect on life and all the beauty that it offers."
+end

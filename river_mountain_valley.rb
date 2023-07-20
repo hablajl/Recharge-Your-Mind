2805e4a07c4b2553f9d8f354e9a7b9442435368a
hablajl <136637926+hablajl@users.noreply.github.com>
2023-07-20 18:05:29 +0700

    Refine user experience

---
+++ river_mountain_valley.rb
@@ -0,0 +1,61 @@
+#1
+def recharge_your_mind
+  puts "Start each morning with a positive affirmation:"
+  puts "I am capable and strong." 
+end
+
+#2
+def create_mindful_habits
+  puts "Find 20 minutes each day to practice meditation or mindfulness exercises."
+  puts "Breathe in slowly for 5 seconds, hold your breath for 5 seconds, and breathe out for 5 seconds. Repeat the cycle."
+end
+
+#3
+def establish_productive_routines
+  puts "Form a set of productive routines that you can stick to every day, such as: Waking up early, taking a walk, drinking more water, or reading."
+end
+
+#4
+def remove_distractions
+  puts "Minimize distractions by disconnecting from social media, turning off your phone, listening to productive music or focusing apps." 
+end
+
+#5
+def brainstorm_new_ideas
+  puts "Take a break and brainstorm new ideas. Take any problem you’re working on and look at it from different perspectives to come up with creative solutions."
+end
+
+#6
+def incorporate_stretching
+  puts "Remember to incorporate stretching into your daily routine. This can help to decrease tension in the body, improve range of motion, and increase circulation to the brain."
+end
+
+#7
+def break_your_routine
+  puts "Mix it up! Leave your workspace to get some fresh air, break up monotonous tasks with more creative activities, and switch up your environment. "
+end
+
+#8
+def make_time_for_fun
+  puts "Schedule some fun into your daily routine. Whether it’s playing a game or practicing a hobby, make time for yourself to relax and recharge."
+end
+
+#9
+def replenish_your_energy
+  puts "Nourish yourself with healthy, energizing snacks that keep your blood sugar levels balanced. Opt for sweet fruits, nuts, or seeds for an afternoon pick me up."
+end
+
+#10
+def go_outside
+  puts "Spend 20-30 minutes outside each day. Connect with nature and take a break from electronics to breathe in some fresh air and soak up some Vitamin D."
+end
+
+#11
+def practice_self_care
+  puts "Don’t forget to practice self-care. Schedule time for yourself to listen to calming music, journal or draw, practice yoga, or take a warm bath."
+end
+
+#12
+def get_adequate_sleep
+  puts "Finally, make sure to get adequate sleep. Aim to get 7-8 hours of restful sleep each night to have the energy you need to recharge your mind."
+end
